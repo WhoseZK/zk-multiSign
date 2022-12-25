d8add16d15f239954c3b6f2d96c847ca8bb619b8
Evis Chang <yschang0926@outlook.com>
2022-12-26 01:52:31 +0800

    add the transfer erc20 token behavior

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -12,6 +12,11 @@ const ABI = [
   "function updatePolynominal(uint256[2] calldata publicSignals, uint256[8] calldata proof) external",
 ];
 
+const ERC20_ABI = [
+  process.env.mockErc20Constructor, 
+  process.env.mockErc20BalanceOf
+];
+
 export default function Home() {
   const { data, error } = useSWR("/api/zkp", fetcher);
   const connect = useMetamask();
@@ -32,6 +37,13 @@ export default function Home() {
   const [amount, setAmount] = useState();
   const [publicSignals, setPublicSignals] = useState([]);
   const [proof, setProof] = useState([]);
+
+  // deploy ERC20 
+  const [mockErc20, setMockErc20] = useState();
+
+  // check wallet amount
+  const [destinationAmt, setDestinationAmt] = useState("Loading"); 
+  const [walletAmt, setWalletAmt] = useState("Loading"); 
 
   useEffect(() => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -81,7 +93,7 @@ export default function Home() {
   // need to write a script on hardhat to
   // deploy verifier & erc20 first or
   // deploy with zk wallet together within deploy func
-  const deploy = async () => {
+  const deployZkWallet = async () => {
     if (localStorage.getItem("zkWallet")) {
       setContract(new ethers.Contract(localStorage.getItem("zkWallet"), ABI, provider.getSigner()));
     } else {
@@ -108,32 +120,83 @@ export default function Home() {
     }
   };
 
+  const deployErc20 = async () => {
+    if (localStorage.getItem("mockErc20")) {
+      const contract = new ethers.Contract(localStorage.getItem("mockErc20"), ERC20_ABI, provider.getSigner())
+      setMockErc20(contract);
+      setTokenAddress(contract.address);
+      getErc20Balance(address, contract).then(it => setWalletAmt(it));
+    } else {
+      const bytecode = process.env.mockErc20ByteCode;
+      const signer = provider.getSigner();
+      const factory = new ethers.ContractFactory(
+        ERC20_ABI,
+        bytecode,
+        signer
+      );
+
+      const contract = await factory.deploy(address);
+      await contract.deployed();
+      contract.connect(signer);
+
+      console.log("deploy mockerc20 at address:", contract.address);
+      localStorage.setItem("mockErc20", contract.address);
+      setMockErc20(contract);
+      setTokenAddress(contract.address);
+      getErc20Balance(address).then(it => setWalletAmt(it));
+    }
+  }
+
   // default setting: transfer ETH
   const tranferToken = async () => {
-    const msg = {
-      value: ethers.BigNumber.from(amount),
-    };
+    const msg = {value: ethers.BigNumber.from(amount), gasLimit: 2000_000}
+
+    var txn;
     if (tokenAddress == ethers.constants.AddressZero) {
-      await contract.transferToken(
-          tokenAddress,
-          destination,
-          amount,
-          publicSignals,
-          proof,
-          msg
-        );
+      txn = await contract.transferToken(
+        tokenAddress,
+        destination,
+        msg.value,
+        publicSignals,
+        proof, 
+        msg
+      );
     } else {
-      await contract.transferToken(
-          tokenAddress,
-          destination,
-          amount,
-          publicSignals,
-          proof,
-          msg
-        );
+      txn = await contract.transferToken(
+        tokenAddress,
+        destination,
+        amount,
+        publicSignals,
+        proof,
+        {
+          gasLimit: 2000_000
+        }
+      );
+      console.log(txn);
+      txn.wait(confirm = 1).then(() => {
+        refreshWalletAmt(true, destination)
+      });
     }
   };
 
+  const refreshWalletAmt = async (success, destination) => {
+    if (success) {
+      getErc20Balance(address).then(it => setWalletAmt(it));
+      getErc20Balance(destination).then(it => setDestinationAmt(it));
+    } else {
+      setWalletAmt("Loading");
+      setDestinationAmt("Loading");
+    }
+  }
+
+  const getErc20Balance = async(address, contract) => {
+
+    const existContract = mockErc20 == null ? contract : mockErc20
+    const result = await existContract.balanceOf(address);
+    console.log(address, result);
+    return parseInt(BigInt(result._hex));
+  }
+ 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -169,8 +232,13 @@ export default function Home() {
 
         <Points points={points} />
         <div className={styles.container}>
-          <button className={styles.button} onClick={deploy}>
+          <button className={styles.button} onClick={deployZkWallet}>
             Create Zk Wallet
+          </button>
+        </div>
+        <div className={styles.container}>
+          <button className={styles.button} onClick={deployErc20}>
+            Create Mock Token
           </button>
         </div>
         <div className={styles.container}>
@@ -205,6 +273,15 @@ export default function Home() {
             Transfer Tokens
           </button>
         </div>
+
+        <div className={styles.container}>
+          <h2>Erc20 Transfer Result: </h2>
+          <label htmlFor="wallet amount">Current Amount: </label>
+          <p>{walletAmt}</p>
+          <label htmlFor="destinationAmt">Destination Amount: </label>
+          <p>{destinationAmt}</p>
+        </div>
+
       </main>
     </div>
   );
